b627fd512c61bba8a4e6e6dfed0e42d92dad65b5
Steven-omeara <Soccerfan580@yahoo.com>
2016-04-11 15:46:44 -0400

    AST is complete
    Added AST, had an issue with If and While statements but fixed them.

--- scripts/tree.js
+++ scripts/tree.js
@@ -101,4 +101,173 @@ function Tree() {
         // Return the result.
         return traversalResult;
     };
+
+    this.DFS = function()
+    {
+        // AST Tree
+
+        var AST = new Tree();
+        var ifwhileCheck = false;
+
+        // Recursive function to handle the expansion of the nodes.
+        function expand(node, depth)
+        {
+            
+            function checkStatementList(node)
+            {
+                if(node.name == "PrintStmt")
+                {
+                    AST.addNode("Print","branch");
+                    checkExpr(node.children[2]);
+                    AST.endChildren();
+                }
+                else if(node.name == "varDecl")
+                {
+                    AST.addNode("varDecl","branch");
+                    AST.addNode(node.children[0].name,"leaf");
+                    AST.addNode(node.children[1].children[0].name,"leaf");
+                    AST.endChildren();
+                }
+                else if(node.name == "Assign")
+                {
+                    //Could also be =, instead of assign
+                    AST.addNode("Assign","branch");
+                    AST.addNode(node.children[0].children[0].name,"leaf");
+                    checkExpr(node.children[2]);
+                    AST.endChildren();
+                }
+                else if(node.name == "If")
+                {
+                    AST.addNode("If","branch");
+                    ifwhileBooleanCheck(node.children[1]);
+                    ifwhileCheck = true;
+                    //addBlock();
+                    //checkStatementList(node.children[2].children[1].children[0]);
+                    //AST.endChildren();
+                    console.log(node.children[2].children[1].children[0].name);
+                }
+                else if(node.name == "While")
+                {
+                    AST.addNode("While","branch");
+                    ifwhileBooleanCheck(node.children[1]);
+                    //addBlock();
+                    //checkStatementList(node.children[2].children[1].children[0]);
+                    //AST.endChildren();
+                    console.log(node.children[2].children[1].children[0].name);
+                }
+                // && notFirst == false
+                else if (node.name == "Block")
+                {
+                    addBlock();
+                    //notFirst = true;
+                }
+            }
+
+            function addBlock()
+            {
+                AST.addNode("Block","branch");
+            }
+
+            function checkExpr(node)
+            {
+                //console.log(node.children[0].name);
+                if(node.children[0].name == "Id")
+                {
+                    AST.addNode(node.children[0].children[0].name,"leaf");
+                }
+                else if(node.children[0].name == "BooleanExpr")
+                {
+                    booleanCheck(node.children[0]);
+                }
+                else if (node.children[0].name == "IntExpr")
+                {
+                    intExprCheck(node.children[0]);
+                }
+                //Change StringExpr to return string not null in lex
+                else if (node.children[0].name == "StringExpr")
+                {
+                    AST.addNode(node.children[0].children[0].name,"leaf");
+                }   
+            }
+            function booleanCheck(node)
+            {
+                if(node.children[0].name == "True" || node.children[0].name == "False")
+                {
+                    AST.addNode(node.children[0].name,"leaf");
+                }
+                else
+                {
+                    //This is currently set to "!="/"==", can change it to compare or whatever if needed
+                    AST.addNode(node.children[2].name,"branch");
+                    checkExpr(node.children[1]);
+                    checkExpr(node.children[3]);
+                    AST.endChildren();
+                }
+            }
+            function ifwhileBooleanCheck(node)
+            {
+                if(node.children[0].name == "True" || node.children[0].name == "False")
+                {
+                    AST.addNode("Cond","branch");
+                    AST.addNode(node.children[0].name,"leaf");
+                    AST.endChildren();
+                }
+                else
+                {
+                    //This is currently set to "!="/"==", can change it to compare or whatever if needed
+                    AST.addNode("Cond","branch");
+                    checkExpr(node.children[1]);
+                    checkExpr(node.children[3]);
+                    AST.endChildren();
+                }
+            }
+            function intExprCheck(node)
+            {
+                if (node.children.length > 1)
+                {
+                    //Currently putting in +, can put in Add if need be
+                    AST.addNode(node.children[1].name,"branch");
+                    AST.addNode(node.children[0].name,"leaf");
+                    checkExpr(node.children[2]);
+                    AST.endChildren();
+                }
+                else
+                {
+                    AST.addNode(node.children[0].name,"leaf");
+                }
+            }
+
+            // If there are no children (i.e., leaf nodes)...
+            if (!node.children || node.children.length === 0)
+            {
+                if(node.name == "}")
+                {
+                    AST.endChildren();
+                    if (ifwhileCheck == true)
+                    {
+                        AST.endChildren();
+                        ifwhileCheck == false;
+                    }
+                }
+            }
+            else
+            {
+                /*if(node.name == "Block")
+                {
+                    AST.addNode("Block","branch");
+                }*/
+                checkStatementList(node);
+                // There are children, so note these interior/branch nodes and ...
+                // .. recursively expand them.
+                for (var i = 0; i < node.children.length; i++)
+                {
+                    expand(node.children[i], depth + 1);
+                }
+            }
+        }
+        // Make the initial call to expand from the root.
+        expand(this.root, 0);
+        // Return the result.
+        return AST;
+    }
 }
